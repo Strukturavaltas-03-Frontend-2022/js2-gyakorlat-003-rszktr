fb705fcc6413c2d3e7410796e92f2da17fbf2b63
Joe <reszketo.robert@gmail.com>
2022-08-19 21:03:17 +0200

    solution

--- solutions/app2.js
+++ solutions/app2.js
@@ -2,3 +2,11 @@ const user = {
   firstName: 'John',
   lastName: 'Doe',
 };
+
+const { firstName: f, lastName: l, job: j = 'unknown' } = user;
+
+export {
+  f,
+  l,
+  j,
+};
--- solutions/app3.js
+++ solutions/app3.js
@@ -1,2 +1,11 @@
+
 const names = ['John', 'Jane', 'Judith', 'Jennifer', 'Jeremiah', 'Johnny'];
+
+const [, a, , b, , c] = names;
+
+export {
+    a,
+    b,
+    c,
+};
 
